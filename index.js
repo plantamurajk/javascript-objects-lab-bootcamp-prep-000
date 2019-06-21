249c84340480ea61d9dc4cc9e0b2faca8fc483c1
John Plantamura <plantamurajk@gmail.com>
2019-06-21 17:07:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,4 +15,5 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 function deleteFromObjectByKey(object, key){
   var clone Object.assign({}, object)
   delete clone[key]
+  return clone
 }

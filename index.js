0709f0959a63db1479455e68fe4d10797a0bba87
John Plantamura <plantamurajk@gmail.com>
2019-06-21 16:07:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var recipes = {}
 
 
 function updateObjectWithKeyAndValue(object, key, value){
-  object[key] = value
-  return object
+  newObject = object
+  newObject[key] = value
+  return newObject
 }
